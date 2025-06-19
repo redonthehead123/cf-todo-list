edf01a43646a08e03247192da398094015751052
redonthehead123 <jordangreer1997@yahoo.com>
2025-06-19 04:25:34 -0500

    Removed Reference Code
    Removed the code I was using as a reference of what jQuery can do and added functions to add, strike, delete and move items on the ordered list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
-let list = $('#list');
-let li = $('<li></li>');
+function newItem() {
+  // Add a new item to the list
+  let inputValue = $('#input').val().trim();
+  if (!inputValue) return;
+  let li = $('<li></li>').text(inputValue);
 
-let inputValue = $('#input').val();
-  li.append(inputValue);
-  list.append(li);
-  //li.addClass("delete");
-
-li.on("click", function() {
-    li.addClass("strike");
+  // Cross out an item from the list upon clicking the item
+  li.on('click', function() {
+    $(this).toggleClass('strike');
   });
+
+  // Delete an item from the list
+  let deleteBtn = $('<button></button>')
+    .text('X')
+    .addClass('crossOutButton')
+    .on('click', function(e) {
+      e.stopPropagation();
+      li.addClass('delete');
+    });
+  
+  li.append(deleteBtn);
+  $('#list').append(li);
+
+  // Make the list sortable
+  $('#list').sortable();
+
+  // Clear input
+  $('#input').val('');
+}
+
+$('#input').keypress(function(e) {
+  if (e.which === 13) {
+    newItem();
+    return false;
+  }
+});
